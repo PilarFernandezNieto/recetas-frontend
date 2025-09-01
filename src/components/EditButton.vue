5d7d7c7e46f8915f0898efe053a03fd4df99f76e
PilarFernandezNieto <apreilmc@gmail.com>
2025-09-01 18:07:39 +0200

    cambio color boton editar

--- src/components/EditButton.vue
+++ src/components/EditButton.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 <template>
   <RouterLink
     :to="to"
-    class="inline-flex items-center justify-center p-1 bg-blue-800 border border-transparent rounded-md font-semibold text-white tracking-widest hover:bg-blue-500 focus:bg-blue-500 active:bg-blue-800 focus:outline-none transition ease-in-out duration-150"
+    class="inline-flex items-center justify-center p-1 bg-blue-500 border border-transparent rounded-md font-semibold text-white tracking-widest hover:bg-blue-800 focus:bg-blue-800 active:bg-blue-800 focus:outline-none transition ease-in-out duration-150"
   >
     <slot />
   </RouterLink>
